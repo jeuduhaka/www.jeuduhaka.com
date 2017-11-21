3200f3cd0749232f5b2496914e836d5a84caaa16
Florent Roques <roques.florent@gmail.com>
2017-11-21 16:26:58 +0100

    update single quotes to double quotes to avoid backslash

--- resources/lang/en/application.php
+++ resources/lang/en/application.php
@@ -1,12 +1,12 @@
 <?php
 return [
-    'metaTitle' => 'The Haka\'s Game Ludocoach free app',
+    'metaTitle' => "The Haka's Game Ludocoach free app",
     'metaDescription' => 'for iPhone and Android',
-    'ogTitle' => 'Download the Haka\'s Game Ludocoach free app!',
+    'ogTitle' => "Download the Haka's Game Ludocoach free app!",
     'ogDescription' => 'on iPhone and Android',
     
-    'hakasGameLudocoach' => 'The Haka\'s Game Ludocoach',
-    'appGoodAndFree' => 'It\'s an app which makes you feel good and it\'s free!',
+    'hakasGameLudocoach' => "The Haka's Game Ludocoach",
+    'appGoodAndFree' => "It's an app which makes you feel good and it's free!",
     'downloadAndShare' => 'Download and share! :)',
     
     'availableAppStore' => 'Available on App Store',
--- resources/lang/fr/application.php
+++ resources/lang/fr/application.php
@@ -2,11 +2,11 @@
 return [
     'metaTitle' => 'Application gratuite Jeu du Haka Ludocoach',
     'metaDescription' => 'pour iPhone et Android',
-    'ogTitle' => 'Téléchargez gratuitement l\'application Jeu du Haka Ludocoach!',
+    'ogTitle' => "Téléchargez gratuitement l'application Jeu du Haka Ludocoach!",
     'ogDescription' => 'sur iPhone et Android',
 
     'hakasGameLudocoach' => 'Le Jeu du Haka Ludocoach',
-    'appGoodAndFree' => 'C\'est une appli qui fait du bien et c\'est gratuit!',
+    'appGoodAndFree' => "C'est une appli qui fait du bien et c'est gratuit!",
     'downloadAndShare' => 'A télécharger et à partager! :)',
 
     'availableAppStore' => 'Disponible sur App Store',
